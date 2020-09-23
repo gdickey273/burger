4df0e1170e5659f75105781e86d3d4a03c503c0a
gdickey273 <gdickey273@gmail.com>
2020-09-23 12:04:02 -0400

    added seeds.sql to populate burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+insert into burgers (burger_name, devoured)
+values("Veggie Burger", true);
+
+insert into burgers (burger_name, devoured)
+values("Fried Egg Burger", true);
+
+insert into burgers (burger_name, devoured)
+values("Bacon Burger", true);
