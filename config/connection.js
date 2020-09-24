257182598ac222126ab3a3478672ea2f8c0c99a3
gdickey273 <gdickey273@gmail.com>
2020-09-24 14:33:52 -0400

    trying to make connection work on heroku

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8889,
   user: "root",
   password: "12345678",
   database: "burgers_db"
